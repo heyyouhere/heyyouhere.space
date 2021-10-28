47181707d87c9f022ebcc6e06c2719e52ab25bc3
Maxim Kalanchekaev <max.kalan@gmail.com>
2021-10-28 22:32:08 +0300

    deleted GETdaemon

--- scripts/PUSHdaemon.js
+++ scripts/PUSHdaemon.js
@@ -1,16 +1,36 @@
 import 'https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js';
+import { verifiedaccess_v1 } from "googleapis";
+import { fstat, writeFile, writeFileSync } from 'fs';
+import 'https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js';
+import pkg from "xhr2";
+const {XMLHttpRequest} = pkg;
 
+
+var requestYT = new XMLHttpRequest();
+var data;
+var ytURL;
 let subs = document.querySelector(".subs");
 let subGoal = document.querySelector(".subgoal");
 let progress = document.querySelector(".progress");
 subGoal.textContent = 10000;
 let doc = "scripts/subINFO.json";
 
+
+$.getJSON(tokens, function(json){
+    ytURL = json.token;
+});
+
 progress.max = subGoal.textContent;
 setInterval(() => {
-    $.getJSON(doc, function(json) {
-        subs.textContent = json.subs;
-        progress.value = Number(json.subs);    
-        console.log(json);
-    });
-}, 1000);
+    requestYT.open('GET', ytURL);
+    requestYT.responseType = 'json';
+    requestYT.send();
+    requestYT.onload = function() {
+        var text = requestYT.response;
+        data = '{ "subs" : '  + text.items[0].statistics.subscriberCount +'}';
+        console.log(data);
+    };
+    writeFileSync('subINFO.json', data);
+    progress.value = Number(data.subs);    
+    console.log(json);
+}, 5000);
