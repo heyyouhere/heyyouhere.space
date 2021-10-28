0d02f7f8fe7ebcd420cee760d01319c43ea15748
Maxim Kalanchekaev <max.kalan@gmail.com>
2021-10-28 23:43:27 +0300

    Update PUSHdaemon.js

--- scripts/PUSHdaemon.js
+++ scripts/PUSHdaemon.js
@@ -3,22 +3,31 @@ import 'https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js';
 
 var data;
 var ytURL;
+let tokens = "/scripts/tokens.json"
+
 
 let subGoal = document.querySelector(".subgoal");
-let progress = document.querySelector(".progress");
 subGoal.textContent = 10000;
-let tokens = "/scripts/tokens.json"
+let progress = document.querySelector(".progress");
+progress.max = subGoal.textContent;
 
 
 $.getJSON(tokens, function(json){
     ytURL = json.url;
 });
 
-progress.max = subGoal.textContent;
+updatePage();
+
 setInterval(() => {
+   updatePage();  
+}, 5000);
+
+
+
+function updatePage(){
     $.getJSON(ytURL, function(ytAnswer){
         data = ytAnswer.items[0].statistics.subscriberCount;
         console.log(data);
     });
     progress.value = Number(data);    
-}, 5000);
+}
